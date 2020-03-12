7e54f996bad14620d87ef16808a81a3ca764052f
AnnouchYassine <Annouch@Yassine.com>
2020-03-12 01:29:36 +0100

    This is my first start in git project

---
+++ GIT-TEST/src/main/java/com/fh2/demo/GitTestApplication.java
@@ -0,0 +1,20 @@
+package com.fh2.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+	}
+
+}

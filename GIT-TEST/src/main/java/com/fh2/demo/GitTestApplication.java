a9f4e5874e5ec3e11fcbb2f355680d0ea1b471b1
AnnouchYassine <Annouch@Yassine.com>
2020-03-18 18:28:52 +0100

    Merge Two Array

--- GIT-TEST/src/main/java/com/fh2/demo/GitTestApplication.java
+++ GIT-TEST/src/main/java/com/fh2/demo/GitTestApplication.java
@@ -1,14 +1,26 @@
 package com.fh2.demo;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitTestApplication implements CommandLineRunner{
+	
+	public static String[] uniqueNames(String[] names1, String[] names2) {
+          
+        Set<String> result = new HashSet<>(Arrays.asList(names1));
+        result.addAll(Arrays.asList(names2));
+		return result.toArray(new String[0]);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitTestApplication.class, args);
+		
 	}
 
 	@Override
@@ -16,6 +28,10 @@ public class GitTestApplication implements CommandLineRunner{
 		System.out.println("This is a just a test !!");
 		
 		
+		
+		String[] names1 = new String[] {"Ava", "Emma", "Olivia"};
+        String[] names2 = new String[] {"Olivia", "Sophia", "Emma"};
+        System.out.println(String.join(", ", uniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
 	}
 
 }

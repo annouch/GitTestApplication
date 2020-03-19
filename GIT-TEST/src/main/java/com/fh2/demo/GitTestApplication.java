a33d6b9d6d22c7babc1aa355c1ea623c729e6466
AnnouchYassine <Annouch@Yassine.com>
2020-03-19 16:11:41 +0100

    Nmbr Le plus proche de Zero

--- GIT-TEST/src/main/java/com/fh2/demo/GitTestApplication.java
+++ GIT-TEST/src/main/java/com/fh2/demo/GitTestApplication.java
@@ -1,14 +1,9 @@
 package com.fh2.demo;
 
-import java.sql.Date;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,56 +11,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitTestApplication implements CommandLineRunner{
 	
+	public static int procheZero(int[] list) {
+		int valeurMin = list[0];
+		for(int i=1; i<list.length; i++) {
+		   if( list[i]<valeurMin) {
+		          valeurMin=list[i];
+		   }
+	}
+		return valeurMin;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitTestApplication.class, args);
 		
 	}
 
-	@Override 
+	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("This is a just a test !!");
-        
+        System.out.println(procheZero(new int[] { 1, 3, -8, -17 }));
 
 		
 		
 	}
 	
-	class AlertService {
-
-		private final AlertDAO storage ; 
-		    
-		    public AlertService(AlertDAO storage){
-		        this.storage=storage;
-		    }
-		   // private final MapAlertDAO storage = new MapAlertDAO();
-				
-		    public UUID raiseAlert() {
-		        return this.storage.addAlert(new Date(0));
-		    }
-			
-		    public Date getAlertTime(UUID id) {
-		        return this.storage.getAlert(id);
-		    }	
-		}
-
-		class MapAlertDAO implements AlertDAO{
-		    private final Map<UUID, Date> alerts = new HashMap<UUID, Date>();
-			@Override
-		    public UUID addAlert(Date time) {
-		    	UUID id = UUID.randomUUID();
-		        this.alerts.put(id, time);
-		        return id;
-		    }
-			@Override
-		    public Date getAlert(UUID id) {
-		        return this.alerts.get(id);
-		    }	
-		}
-		interface AlertDAO {
-		    public UUID addAlert(Date time);
-		     public Date getAlert(UUID id);
-		}	
+	
 
     
 
